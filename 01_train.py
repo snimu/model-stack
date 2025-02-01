81904c9fef99b51beadbde412dc4288f11ca61af
snimu <sebastian.nicolas.mueller@gmail.com>
2025-02-01 22:53:05 +0100

    fix: correct defaults

--- 01_train.py
+++ 01_train.py
@@ -374,8 +374,8 @@ class DistributedDataLoader:
 
 def train(
         # data hyperparams
-        input_bin : str = 'fineweb10B/fineweb_train_*.bin', # input .bin to train on
-        input_val_bin : str = 'fineweb10B/fineweb_val_*.bin', # input .bin to eval validation loss on
+        input_bin : str = 'fineweb100B/fineweb_train_*.bin', # input .bin to train on
+        input_val_bin : str = 'fineweb100B/fineweb_val_*.bin', # input .bin to eval validation loss on
         # optimization hyperparams
         batch_size : int = 8*64, # batch size, in sequences, across all devices
         device_batch_size : int = 64, # batch size, in sequences, per device
