9dcfabfaecf63c72a803d491b8babf542b684b8b
snimu <sebastian.nicolas.mueller@gmail.com>
2025-02-04 07:14:49 +0100

    fix: stack the correct models

--- ablations.sh
+++ ablations.sh
@@ -9,8 +9,8 @@ torchrun --nproc_per_node=4 01_train.py --train --model-id=model1-shared-embeddi
 torchrun --nproc_per_node=4 01_train.py --train --model-id=model2-shared-embeddings --from-model=logs/model1-shared-embeddings/final_state.pt
 ## STACK & EVAL
 ### two different models
-torchrun --nproc_per_node=4 01_train.py --model-names logs/model1-shared-embeddings/final_state.pt logs/model2/final_state.pt --savefile=2models-no-first-layer-shared-embeddings
-torchrun --nproc_per_node=4 01_train.py --model-names logs/model1-shared-embeddings/final_state.pt logs/model2/final_state.pt --use-first-layer --savefile=2models-first-layer-shared-embeddings
+torchrun --nproc_per_node=4 01_train.py --model-names logs/model1-shared-embeddings/final_state.pt logs/model2-shared-embeddings/final_state.pt --savefile=2models-no-first-layer-shared-embeddings
+torchrun --nproc_per_node=4 01_train.py --model-names logs/model1-shared-embeddings/final_state.pt logs/model2-shared-embeddings/final_state.pt --use-first-layer --savefile=2models-first-layer-shared-embeddings
 ### baseline: same model twice
-torchrun --nproc_per_node=4 01_train.py --model-names logs/model1-shared-embeddings/final_state.pt logs/model1/final_state.pt --savefile=1model-twice-no-first-layer-shared-embeddings
-torchrun --nproc_per_node=4 01_train.py --model-names logs/model1-shared-embeddings/final_state.pt logs/model1/final_state.pt --use-first-layer --savefile=1model-twice-first-layer-shared-embeddings
+torchrun --nproc_per_node=4 01_train.py --model-names logs/model1-shared-embeddings/final_state.pt logs/model1-shared-embeddings/final_state.pt --savefile=1model-twice-no-first-layer-shared-embeddings
+torchrun --nproc_per_node=4 01_train.py --model-names logs/model1-shared-embeddings/final_state.pt logs/model1-shared-embeddings/final_state.pt --use-first-layer --savefile=1model-twice-first-layer-shared-embeddings
