fa66a6e54d3f33c22bee370cdde0a6be9dcd6167
snimu <sebastian.nicolas.mueller@gmail.com>
2025-02-25 21:44:54 +0100

    ablations!!!1

--- ablations1xtoks-norm.sh
+++ ablations1xtoks-norm.sh
@@ -2,7 +2,7 @@
 torchrun --nproc_per_node=8 01_train.py --train --model-id=m1-shared-emb-1xtoks-norm --seed=399938282
 torchrun --nproc_per_node=8 01_train.py --train --model-id=m2-shared-emb-1xtoks-norm --seed=677719191 --from-model=logs/m1-shared-emb-1xtoks-norm/final_state.pt 
 
-# STACK & EVAL
+# STACK & EVAL WITH NORMALIZATION
 ## two different models
 torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm
 torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-first-layer
@@ -13,3 +13,15 @@ torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-
 torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-first-layer
 torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-last-layer
 torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-first-layer --use-last-layer
+
+# STACK & EVAL WITHOUT NORMALIZATION
+## two different models
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --savefile=results_1xtoks_norm
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --savefile=results_1xtoks_norm --use-first-layer
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --savefile=results_1xtoks_norm --use-last-layer
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --savefile=results_1xtoks_norm --use-first-layer --use-last-layer
+## baseline: same model twice
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --savefile=results_1xtoks_norm
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --savefile=results_1xtoks_norm --use-first-layer
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --savefile=results_1xtoks_norm --use-last-layer  
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --savefile=results_1xtoks_norm --use-first-layer --use-last-layer
--- ablations5xtoks.sh
+++ ablations5xtoks.sh
@@ -1,15 +1,15 @@
 # TRAIN
-torchrun --nproc_per_node=4 01_train.py --train --model-id=m1-shared-emb-5xtoks --seed=98765432 --num-iterations 31000 --weight-decay=0.01 --warmdown-iters 9000
-torchrun --nproc_per_node=4 01_train.py --train --model-id=m2-shared-emb-5xtoks --seed=84759274 --num-iterations 31000 --weight-decay=0.01 --warmdown-iters 9000 --from-model=logs/m1-shared-emb-5xtoks/final_state.pt
+torchrun --nproc_per_node=8 01_train.py --train --model-id=m1-shared-emb-5xtoks --seed=98765432 --num-iterations 31000 --weight-decay=0.01 --warmdown-iters 9000
+torchrun --nproc_per_node=8 01_train.py --train --model-id=m2-shared-emb-5xtoks --seed=84759274 --num-iterations 31000 --weight-decay=0.01 --warmdown-iters 9000 --from-model=logs/m1-shared-emb-5xtoks/final_state.pt
 
 # STACK & EVAL
 ## two different models
-torchrun --nproc_per_node=4 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m2-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks
-torchrun --nproc_per_node=4 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m2-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks --use-first-layer
-torchrun --nproc_per_node=4 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m2-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks --use-last-layer
-torchrun --nproc_per_node=4 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m2-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks --use-first-layer --use-last-layer
+torchrun --nproc_per_node=8 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m2-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks
+torchrun --nproc_per_node=8 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m2-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks --use-first-layer
+torchrun --nproc_per_node=8 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m2-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks --use-last-layer
+torchrun --nproc_per_node=8 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m2-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks --use-first-layer --use-last-layer
 ## baseline: same model twice
-torchrun --nproc_per_node=4 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m1-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks
-torchrun --nproc_per_node=4 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m1-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks --use-first-layer
-torchrun --nproc_per_node=4 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m1-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks --use-last-layer
-torchrun --nproc_per_node=4 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m1-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks --use-first-layer --use-last-layer
+torchrun --nproc_per_node=8 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m1-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks
+torchrun --nproc_per_node=8 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m1-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks --use-first-layer
+torchrun --nproc_per_node=8 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m1-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks --use-last-layer
+torchrun --nproc_per_node=8 01_train.py --num-iterations 31000 --model-names logs/m1-shared-emb-5xtoks/final_state.pt logs/m1-shared-emb-5xtoks/final_state.pt --weight-decay=0.01 --savefile=results_5xtoks --use-first-layer --use-last-layer
