cc746691c0bf0c648207d32121936cb4dab28bca
snimu <sebastian.nicolas.mueller@gmail.com>
2025-02-25 21:38:18 +0100

    actually use 8 h100s...

--- ablations1xtoks-norm.sh
+++ ablations1xtoks-norm.sh
@@ -1,15 +1,15 @@
 # TRAIN
-torchrun --nproc_per_node=4 01_train.py --train --model-id=m1-shared-emb-1xtoks-norm --seed=399938282
-torchrun --nproc_per_node=4 01_train.py --train --model-id=m2-shared-emb-1xtoks-norm --seed=677719191 --from-model=logs/m1-shared-emb-1xtoks-norm/final_state.pt 
+torchrun --nproc_per_node=8 01_train.py --train --model-id=m1-shared-emb-1xtoks-norm --seed=399938282
+torchrun --nproc_per_node=8 01_train.py --train --model-id=m2-shared-emb-1xtoks-norm --seed=677719191 --from-model=logs/m1-shared-emb-1xtoks-norm/final_state.pt 
 
 # STACK & EVAL
 ## two different models
-torchrun --nproc_per_node=4 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm
-torchrun --nproc_per_node=4 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-first-layer
-torchrun --nproc_per_node=4 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-last-layer
-torchrun --nproc_per_node=4 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-first-layer --use-last-layer
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-first-layer
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-last-layer
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m2-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-first-layer --use-last-layer
 ## baseline: same model twice
-torchrun --nproc_per_node=4 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm
-torchrun --nproc_per_node=4 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-first-layer
-torchrun --nproc_per_node=4 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-last-layer
-torchrun --nproc_per_node=4 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-first-layer --use-last-layer
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-first-layer
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-last-layer
+torchrun --nproc_per_node=8 01_train.py --model-names logs/m1-shared-emb-1xtoks-norm/final_state.pt logs/m1-shared-emb-1xtoks-norm/final_state.pt --use-norm --savefile=results_1xtoks_norm --use-first-layer --use-last-layer
