4e64362181d7ac3936497361d3382311c3d61626
snimu <sebastian.nicolas.mueller@gmail.com>
2025-03-09 19:04:50 +0100

    more ablations

--- ablations1xtoks-coconut.sh
+++ ablations1xtoks-coconut.sh
@@ -1,12 +1,23 @@
 # TRAIN
-torchrun --nproc_per_node=8 01_train.py --train --coconut-every 100 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m1-rr-coconut --seed=47747474
-torchrun --nproc_per_node=8 01_train.py --train --coconut-every 100 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m2-rr-coconut --seed=83838392 --from-model=m1-rr-coconut
+torchrun --nproc_per_node=8 01_train.py --train --coconut-every 100 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m1-rr-coconut-every-100 --seed=47747474
+torchrun --nproc_per_node=8 01_train.py --train --coconut-every 100 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m2-rr-coconut-every-100 --seed=83838392 --from-model=m1-rr-coconut-every-100
+torchrun --nproc_per_node=8 01_train.py --train --coconut-every 10 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m3-rr-coconut-every-10 --seed=789363
+torchrun --nproc_per_node=8 01_train.py --train --coconut-every 10 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m4-rr-coconut-every-10 --seed=223002 --from-model=m1-rr-coconut-every-10
+
 
 # STACK & EVAL
-# Norm-less models without norm
-torchrun --nproc_per_node=8 01_train.py --model-names m1-rr-coconut m2-rr-coconut --norm-wte none --norm-lm-head rms_norm --norm-inter-model none --savefile=results_coconut --use-first-layer
-torchrun --nproc_per_node=8 01_train.py --model-names m1-rr-coconut m2-rr-coconut --norm-wte none --norm-lm-head rms_norm --norm-inter-model none --savefile=results_coconut --use-first-layer --use-last-layer
+# Two models, coconut every 100 steps
+torchrun --nproc_per_node=8 01_train.py --model-names m1-rr-coconut-every-100 m2-rr-coconut-every-100 --norm-wte none --norm-lm-head rms_norm --norm-inter-model none --savefile=results_coconut --use-first-layer
+torchrun --nproc_per_node=8 01_train.py --model-names m1-rr-coconut-every-100 m2-rr-coconut-every-100 --norm-wte none --norm-lm-head rms_norm --norm-inter-model none --savefile=results_coconut --use-first-layer --use-last-layer
 
 # Baseline: same model twice
-torchrun --nproc_per_node=8 01_train.py --model-names m1-rr-coconut m2-rr-coconut --norm-wte none --norm-lm-head rms_norm --norm-inter-model none --savefile=results_coconut --use-first-layer
-torchrun --nproc_per_node=8 01_train.py --model-names m1-rr-coconut m2-rr-coconut --norm-wte none --norm-lm-head rms_norm --norm-inter-model none --savefile=results_coconut --use-first-layer --use-last-layer
+torchrun --nproc_per_node=8 01_train.py --model-names m1-rr-coconut-every-100 m2-rr-coconut-every-100 --norm-wte none --norm-lm-head rms_norm --norm-inter-model none --savefile=results_coconut --use-first-layer
+torchrun --nproc_per_node=8 01_train.py --model-names m1-rr-coconut-every-100 m2-rr-coconut-every-100 --norm-wte none --norm-lm-head rms_norm --norm-inter-model none --savefile=results_coconut --use-first-layer --use-last-layer
+
+# Two models, coconut every 10 steps
+torchrun --nproc_per_node=8 01_train.py --model-names m1-rr-coconut-every-10 m2-rr-coconut-every-10 --norm-wte none --norm-lm-head rms_norm --norm-inter-model none --savefile=results_coconut --use-first-layer
+torchrun --nproc_per_node=8 01_train.py --model-names m1-rr-coconut-every-10 m2-rr-coconut-every-10 --norm-wte none --norm-lm-head rms_norm --norm-inter-model none --savefile=results_coconut --use-first-layer --use-last-layer
+
+# Baseline: same model twice
+torchrun --nproc_per_node=8 01_train.py --model-names m1-rr-coconut-every-10 m2-rr-coconut-every-10 --norm-wte none --norm-lm-head rms_norm --norm-inter-model none --savefile=results_coconut --use-first-layer
+torchrun --nproc_per_node=8 01_train.py --model-names m1-rr-coconut-every-10 m2-rr-coconut-every-10 --norm-wte none --norm-lm-head rms_norm --norm-inter-model none --savefile=results_coconut --use-first-layer --use-last-layer
