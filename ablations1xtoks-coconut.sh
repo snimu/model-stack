050ec4ad78a6c214ffa90a7e2f2ac737b4e96015
snimu <sebastian.nicolas.mueller@gmail.com>
2025-03-09 20:30:29 +0100

    fix: correct model names

--- ablations1xtoks-coconut.sh
+++ ablations1xtoks-coconut.sh
@@ -1,8 +1,8 @@
 # TRAIN
 torchrun --nproc_per_node=8 01_train.py --train --coconut-every 100 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m1-rr-coconut-every-100 --seed=47747474
 torchrun --nproc_per_node=8 01_train.py --train --coconut-every 100 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m2-rr-coconut-every-100 --seed=83838392 --from-model=m1-rr-coconut-every-100
-torchrun --nproc_per_node=8 01_train.py --train --coconut-every 10 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m3-rr-coconut-every-10 --seed=789363
-torchrun --nproc_per_node=8 01_train.py --train --coconut-every 10 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m4-rr-coconut-every-10 --seed=223002 --from-model=m1-rr-coconut-every-10
+torchrun --nproc_per_node=8 01_train.py --train --coconut-every 10 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m1-rr-coconut-every-10 --seed=789363
+torchrun --nproc_per_node=8 01_train.py --train --coconut-every 10 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m2-rr-coconut-every-10 --seed=223002 --from-model=m1-rr-coconut-every-10
 
 
 # STACK & EVAL
