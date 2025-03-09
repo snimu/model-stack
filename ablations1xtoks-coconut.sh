b399b5c3687c2b79262a2548743441efc6a76cca
snimu <sebastian.nicolas.mueller@gmail.com>
2025-03-09 18:49:10 +0100

    fix: use of cli

--- ablations1xtoks-coconut.sh
+++ ablations1xtoks-coconut.sh
@@ -1,6 +1,6 @@
 # TRAIN
-torchrun --nproc_per_node=8 01_train.py --train --coconut-every 100 --detach-output-latents --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m1-rr-coconut --seed=47747474
-torchrun --nproc_per_node=8 01_train.py --train --coconut-every 100 --detach-output-latents --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m2-rr-coconut --seed=83838392 --from-model=m1-rr-coconut
+torchrun --nproc_per_node=8 01_train.py --train --coconut-every 100 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m1-rr-coconut --seed=47747474
+torchrun --nproc_per_node=8 01_train.py --train --coconut-every 100 --norm-wte rms_norm --norm-lm-head rms_norm --model-id=m2-rr-coconut --seed=83838392 --from-model=m1-rr-coconut
 
 # STACK & EVAL
 # Norm-less models without norm
